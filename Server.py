b25eaecb31557868f2634ab028078f8eb68ef5b4
hongbuly <60245930+hongbuly@users.noreply.github.com>
2023-11-09 15:43:06 +0900

    add images

--- Server.py
+++ Server.py
@@ -2,13 +2,15 @@ import socket
 from _thread import *
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(300, 300, 400, 400)
+        # self.setStyleSheet("background-color: white;")
+        self.setGeometry(300, 300, 400, 300)
+        self.setFixedSize(QSize(400, 300))
         
         self.count = 0
         btn_socket = QPushButton("Socket", self)
@@ -20,14 +22,19 @@ class MyWindow(QWidget):
         btn_waiting = QPushButton("Waiting", self)
         btn_waiting.clicked.connect(self.btn_waiting_clicked)
 
-        layout = QHBoxLayout()
-        layout.addStretch(1)
-        layout.addWidget(btn_socket)
-        layout.addWidget(btn_bind)
-        layout.addWidget(btn_listen)
-        layout.addWidget(btn_waiting)
-        layout.addStretch(1)
-        self.setLayout(layout)
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(btn_socket)
+        hbox.addWidget(btn_bind)
+        hbox.addWidget(btn_listen)
+        hbox.addWidget(btn_waiting)
+        hbox.addStretch(1)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(6)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+        self.setLayout(vbox)
 
         self.client_sockets = []
 
@@ -40,20 +47,88 @@ class MyWindow(QWidget):
         cloud_scaled_pixmap = cloud_pixmap.scaled(50, 50)  # 원하는 크기로 조절
         self.server_label = QLabel(self)
         self.server_label.setPixmap(cloud_scaled_pixmap)
-        self.server_label.setGeometry(25, 100, 50, 50)
-        # ip/port를 보여줄 텍스트 
+        self.server_label.setGeometry(25, 80, 50, 50)
+        # Server Text
         self.serverT_label = QLabel(self)
         self.serverT_label.setText("<b>Server</b>")
         self.serverT_label.setAlignment(Qt.AlignmentFlag.AlignCenter)  # 가운데 정렬
-        self.serverT_label.setGeometry(25, 125, 50, 50)  # 위치 및 크기 조절
+        self.serverT_label.setGeometry(25, 105, 50, 50)  # 위치 및 크기 조절
         
         # 소켓 아이콘 표시를 위한 QLabel 위젯 생성
         doorC_pixmap = QPixmap('./Images/door_close.png')
         doorC_scaled_pixmap = doorC_pixmap.scaled(50, 50)  # 원하는 크기로 조절
         self.socket_label = QLabel(self)
         self.socket_label.setPixmap(doorC_scaled_pixmap)
-        self.socket_label.setGeometry(25, 55, 50, 50)
+        self.socket_label.setGeometry(25, 35, 50, 50)
         self.socket_label.setVisible(False)
+
+        # Client 아이콘 표시를 위한 3개의 QLabel 위젯 생성
+        client_pixmap = QPixmap('./Images/client.png')
+        client_scaled_pixmap = client_pixmap.scaled(40, 40)  # 원하는 크기로 조절
+        self.client_label1 = QLabel(self)
+        self.client_label1.setPixmap(client_scaled_pixmap)
+        self.client_label1.setGeometry(300, 30, 40, 40)
+        # self.client_label1.setVisible(False)
+
+        self.client_label2 = QLabel(self)
+        self.client_label2.setPixmap(client_scaled_pixmap)
+        self.client_label2.setGeometry(300, 80, 40, 40)
+        # self.client_label2.setVisible(False)
+
+        self.client_label3 = QLabel(self)
+        self.client_label3.setPixmap(client_scaled_pixmap)
+        self.client_label3.setGeometry(300, 130, 40, 40)
+        # self.client_label3.setVisible(False)
+
+        # Client Text 
+        self.serverT_label = QLabel(self)
+        self.serverT_label.setText("<b>Client</b>")
+        self.serverT_label.setAlignment(Qt.AlignmentFlag.AlignCenter)  # 가운데 정렬
+        self.serverT_label.setGeometry(295, 160, 50, 50)  # 위치 및 크기 조절
+
+        # arrow 아이콘 표시를 위한 3개의 QLabel 위젯 생성
+        arrow_pixmap = QPixmap('./Images/arrow1.png')
+        arrow_scaled_pixmap = arrow_pixmap.scaled(560, 30)  # 원하는 크기로 조절
+        self.arrow_label1 = QLabel(self)
+        self.arrow_label1.setPixmap(arrow_scaled_pixmap)
+        self.arrow_label1.setGeometry(80, 90, 200, 30)
+        # self.message_label1.setVisible(False)
+
+        arrow2_pixmap = QPixmap('./Images/arrow2.png')
+        arrow2_scaled_pixmap = arrow2_pixmap.scaled(560, 90)  # 원하는 크기로 조절
+        self.arrow_label2 = QLabel(self)
+        self.arrow_label2.setPixmap(arrow2_scaled_pixmap)
+        self.arrow_label2.setGeometry(80, 0, 200, 90)
+        # self.message_label1.setVisible(False)
+
+        arrow3_pixmap = QPixmap('./Images/arrow3.png')
+        arrow3_scaled_pixmap = arrow3_pixmap.scaled(560, 90)  # 원하는 크기로 조절
+        self.arrow_label3 = QLabel(self)
+        self.arrow_label3.setPixmap(arrow3_scaled_pixmap)
+        self.arrow_label3.setGeometry(80, 120, 200, 90)
+        # self.message_label1.setVisible(False)
+
+        # message 아이콘 표시를 위한 3개의 QLabel 위젯 생성
+        message_pixmap = QPixmap('./Images/msg_g.png')
+        message_scaled_pixmap = message_pixmap.scaled(30, 30)  # 원하는 크기로 조절
+        self.message_label1 = QLabel(self)
+        self.message_label1.setPixmap(message_scaled_pixmap)
+        self.message_label1.setGeometry(270, 40, 30, 30)
+        # self.message_label1.setVisible(False)
+
+        message_r_pixmap = QPixmap('./Images/msg_r.png')
+        message_r_scaled_pixmap = message_r_pixmap.scaled(30, 30)  # 원하는 크기로 조절
+        self.message_label2 = QLabel(self)
+        self.message_label2.setPixmap(message_r_scaled_pixmap)
+        self.message_label2.setGeometry(270, 90, 30, 30)
+        # self.message_label2.setVisible(False)
+
+        message_y_pixmap = QPixmap('./Images/msg_y.png')
+        message_y_scaled_pixmap = message_y_pixmap.scaled(30, 30)  # 원하는 크기로 조절
+        self.message_label3 = QLabel(self)
+        self.message_label3.setPixmap(message_y_scaled_pixmap)
+        self.message_label3.setGeometry(270, 140, 30, 30)
+        # self.message_label3.setVisible(False)
 
         # ip/port를 보여줄 텍스트 
         self.IP_PORT_label = QLabel(self)
@@ -74,7 +149,7 @@ class MyWindow(QWidget):
             combined_text = "<b>IP :" + self.HOST + "<br>Port : "+ str(self.PORT) + "</b>"
             self.IP_PORT_label.setText(combined_text)
             self.IP_PORT_label.setAlignment(Qt.AlignmentFlag.AlignCenter)  # 가운데 정렬
-            self.IP_PORT_label.setGeometry(10, 20, 100, 30)  # 위치 및 크기 조절
+            self.IP_PORT_label.setGeometry(10, 0, 100, 30)  # 위치 및 크기 조절
             
     def btn_listen_clicked(self):
         if self.count == 2:
@@ -123,6 +198,10 @@ class MyWindow(QWidget):
                     self.client_sockets.append(client_socket)
                     start_new_thread(self.threaded, (client_socket, addr))
                     print("참가자 수 : ", len(self.client_sockets))
+                    
+                    #최대 3개의 Client만 접속 가능
+                    while len(self.client_sockets) == 3:
+                        pass
             except Exception as e:
                 print('에러 : ', e)
 
